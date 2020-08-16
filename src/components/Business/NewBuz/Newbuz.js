6ee6e085023f099d641cad07b28f4cdc27088c45
Youssef <proudmoroccan@gmail.com>
2020-08-16 07:30:10 +0000

    jdjdj

--- src/components/Business/NewBuz/Newbuz.js
+++ src/components/Business/NewBuz/Newbuz.js
@@ -5,22 +5,23 @@ import styles from "./Newbuz.module.scss"
 import Spinner from "../../UI/Spinner/Spinner"
 import { CREATE_BUSINESS } from "../../../qraphQl/businessType"
 import { loginRequired } from "../../../AccessToken"
-import { DispatchContext } from "../../../Context"
+import { DispatchContext, StateContext } from "../../../Context"
 import Page from "../../Page/Page"
 
 const NewBuz = props => {
   const appDispatch = useContext(DispatchContext)
+  const appState = useContext(StateContext)
 
   loginRequired(props.history, appDispatch)
 
   const [createBusiness, { loading }] = useMutation(CREATE_BUSINESS, {
     onCompleted({ createBusiness }) {
       appDispatch({ type: "flashMessage", value: { message: `Business ${createBusiness.name} is created`, type: "success" } })
-      window.scrollTo(0, 0)
+      props.history.push(`/profile/${appState.user.userId}`)
     },
     onError(err) {
       console.log(err.message)
-      appDispatch({ type: "flashMessage", value: { message: "Somethicng is wrong please try again later", type: "error" } })
+      appDispatch({ type: "flashMessage", value: { message: err.message.replace("GraphQL error:", ""), type: "error" } })
       window.scrollTo(0, 0)
     }
   })
